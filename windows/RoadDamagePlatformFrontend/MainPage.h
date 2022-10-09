43c1fde6ee8bb1dd10246669524d0c7322e982de
Peter Pei <pyb0924@sjtu.edu.cn>
2022-10-09 17:03:02 +0800

    project init

---
+++ windows/RoadDamagePlatformFrontend/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::RoadDamagePlatformFrontend::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::RoadDamagePlatformFrontend::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
