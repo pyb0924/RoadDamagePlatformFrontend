1e3bff5d1db82f9906c630330508e292f7e7e331
Peter Pei <pyb0924@sjtu.edu.cn>
2022-10-14 17:37:32 +0800

    move code to app/ add redux and formik support

--- index.web.js
+++ index.web.js
@@ -1,6 +1,7 @@
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-import App from './App';
+import App from './app/App';
+
 if (module.hot) {
   module.hot.accept();
 }
