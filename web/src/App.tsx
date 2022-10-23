8b7725c77a782fba9109a7783291e4269f25a58f
Levein7 <83186040@qq.com>
2022-10-23 16:20:24 +0800

    change path: ".../login" to path: "../"

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,7 +7,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
-    path: "/login",
+    path: "/",
     element: <Login />,
   },
   {
