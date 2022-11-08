9e03d7b1fc49c78d7941e463fc08f0778332dff9
Peter Pei <pyb0924@sjtu.edu.cn>
2022-11-08 19:33:56 +0800

    fix bug: Amap not defined

--- web/src/routers/main/map/index.tsx
+++ web/src/routers/main/map/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import { Button } from "antd";
+
 import {
   Map,
   APILoader,
@@ -9,56 +11,55 @@ import {
   Marker,
   InfoWindow,
 } from "@uiw/react-amap";
-import { Button } from "antd";
 
-export default function MapPage() {
-  const [isShowMarker, setIsShowMarker] = useState(true);
+function MapView() {
   const [isShowInfoWindow, setIsShowInfoWindow] = useState(false);
-
   const [infoWindowPosition, setInfoWindowPosition] = useState(
     new AMap.LngLat(121.437, 31.026)
   );
+  return (
+    <Map center={[121.437, 31.026]} zoom={15}>
+      <>
+        <ScaleControl offset={[16, 30]} position="LB" />
+        <ToolBarControl offset={[16, 10]} position="RB" />
+        <ControlBarControl offset={[16, 180]} position="RB" />
+        <Marker
+          title="北京市"
+          position={new AMap.LngLat(116.415285, 39.905589)}
+          onClick={(evn) => {
+            if (!isShowInfoWindow) {
+              setInfoWindowPosition(new AMap.LngLat(116.415285, 39.905589));
+              setIsShowInfoWindow(true);
+            } else {
+              setInfoWindowPosition(new AMap.LngLat(116.415285, 39.905589));
+            }
+          }}
+        />
+        {infoWindowPosition && (
+          <InfoWindow
+            visiable={isShowInfoWindow}
+            position={infoWindowPosition}
+            offset={new window.AMap.Pixel(0, -10)}
+            content={"<div>高德软件</div>"}
+            onClose={(evn) => {
+              console.log("evn2", evn);
+            }}
+          />
+        )}
+      </>
+    </Map>
+  );
+}
 
+export default function MapPage() {
+  const [isShowMarker, setIsShowMarker] = useState(true);
   return (
     <>
       <Button onClick={() => setIsShowMarker(!isShowMarker)}>
         显示/隐藏Marker
       </Button>
       <APILoader akay={process.env.REACT_APP_AMAPKEY}>
-        <Map center={[121.437, 31.026]} zoom={15}>
-          <>
-            <ScaleControl offset={[16, 30]} position="LB" />
-            <ToolBarControl offset={[16, 10]} position="RB" />
-            <ControlBarControl offset={[16, 180]} position="RB" />
-            <Marker
-              title="北京市"
-              position={new AMap.LngLat(116.415285, 39.905589)}
-              onClick={(evn) => {
-                if (!isShowInfoWindow) {
-                  setInfoWindowPosition(
-                    new AMap.LngLat(116.415285, 39.905589)
-                  );
-                  setIsShowInfoWindow(true);
-                } else {
-                  setInfoWindowPosition(
-                    new AMap.LngLat(116.415285, 39.905589)
-                  );
-                }
-              }}
-            />
-            {infoWindowPosition && (
-              <InfoWindow
-                visiable={isShowInfoWindow}
-                position={infoWindowPosition}
-                offset={new window.AMap.Pixel(0, -10)}
-                content={"<div>高德软件</div>"}
-                onClose={(evn) => {
-                  // console.log('evn2', evn, show);
-                }}
-              />
-            )}
-          </>
-        </Map>
+        <MapView />
       </APILoader>
     </>
   );
