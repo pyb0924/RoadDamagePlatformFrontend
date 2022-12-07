3f42d5764a043ed1b885c14255d3c51b1b464d59
Levein7 <83186040@qq.com>
2022-12-07 14:11:59 +0800

    code refinement & "only myself" function in event page

---
+++ web/src/utils/constants.ts
@@ -0,0 +1,16 @@
+import {EventStatus, EventType} from '../store/types/event';
+
+export const eventStatusList = [
+  {key: 'withdraw', name: EventStatus.WITHDRAW, title: '无需养护'},
+  {key: 'onconfirm', name: EventStatus.ONCONFIRM, title: '待确认'},
+  {key: 'onconserve', name: EventStatus.ONCONSERVE, title: '待养护'},
+  {key: 'conserving', name: EventStatus.CONSERVING, title: '养护中'},
+  {key: 'oncheck', name: EventStatus.ONCHECK, title: '待验收'},
+  {key: 'checked', name: EventStatus.CHECKED, title: '已验收'},
+];
+
+export const eventTypeList = [
+  // {key: 'uncatelogued', name: EventType.UNCATELOGUED, title: '未分类'},
+  {key: 'hole', name: EventType.HOLE, title: '坑洞'},
+  {key: 'crack', name: EventType.CRACK, title: '裂痕'},
+];
